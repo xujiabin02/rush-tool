6ae1d62c9689eb7f03874fe8d62b1b3b5333638a
xujiabin <xujiabin@growingio.com>
2022-01-26 11:38:02 +0800

    local

---
+++ getLocal.go
@@ -0,0 +1,22 @@
+package rushtool
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func GetLocal() {
+	addrs,err:=net.InterfaceAddrs()
+	if err!=nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _,address:=range addrs{
+		if ipnet,ok:=address.(*net.IPNet);ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() !=nil{
+				fmt.Println(ipnet.IP.String())
+			}
+		}
+	}
+}
