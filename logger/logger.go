de287e906f919127ec90e740c9c99b8219710423
xujiabin <xujiabin@haima.me>
2023-04-21 16:00:24 +0800

    logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,11 +1,11 @@
-package log
+package logger
 
 import (
 	"log"
 	"os"
 )
 
-var logger = log.New(
+var Logger = log.New(
 	os.Stderr,
 	"service: ",
 	//log.Ldate|log.Ltime|log.Lmicroseconds|log.LUTC|log.Lshortfile,
